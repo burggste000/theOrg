b4172f43eaef907674076883cacead46b8c636af
burggste000 <steedsranch@gmail.com>
2022-02-14 15:05:10 -0700

    added colors and dynamic buttons on header

---
+++ the-react/src/components/App/App.js
@@ -0,0 +1,38 @@
+import './App.css';
+import React from"react";
+//The commented stuff, Dad had me put in to have the server interactable with the front-end.
+export default function App() {
+    // const[data,setData]=React.useState(null);
+    // React.useEffect(()=>{
+    //     fetch("/users")
+    //     .then((res)=>res.json())
+    //     .then((json)=>setData(json));
+    // });
+  return(
+        <header>
+            <input type="file" id="fileUpload" />
+            <input type="button" id="upload" class="menu"value="Upload" />
+            <input id="addBtn" type="button" value="Add Employee" class="menu" />
+            <input type="button" value="Delete Employee" class="menu" />
+            <a href="/api/csv"id="downloadCSV"class="menu"download="org.csv">Download CSV</a>
+        </header>
+
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+    //   <p>{!data?"Loading...":"name: "+data.name}</p>
+    // </div>
+  );
+}
