bab0df12064a7ca33c10dd2b8dba57c5abf3c2f9
burggste000 <steedsranch@gmail.com>
2022-02-15 15:58:40 -0700

    BUTTONS ARE CENTERED GOODgit add .

--- the-react/src/components/App/App.js
+++ the-react/src/components/App/App.js
@@ -10,11 +10,13 @@ export default function App() {
     // });
   return(
         <header>
+            <div id="btnDiv">
             <input type="file" id="fileUpload" />
             <input type="button" id="upload" class="menu"value="Upload" />
             <input id="addBtn" type="button" value="Add Employee" class="menu" />
             <input type="button" value="Delete Employee" class="menu" />
             <a href="/api/csv"id="downloadCSV"class="menu"download="org.csv">Download</a>
+            </div>
         </header>
 
     // <div className="App">
