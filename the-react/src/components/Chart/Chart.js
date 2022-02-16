5389bc65b223318baeb901cbddb6aeb9f0e23b94
burggste000 <steedsranch@gmail.com>
2022-02-15 21:57:34 -0700

    FULL STACK APP WORKING

--- the-react/src/components/Chart/Chart.js
+++ the-react/src/components/Chart/Chart.js
@@ -3,17 +3,37 @@ import React from "react";
 
 const Chart=()=>{
     const[employees,setEmployees]=React.useState([]);
+    const updateMans=empAr=>{
+        for(let element of empAr){
+            const managerr=empAr.find(e=>e.id==element.managerId);
+            if(managerr){
+                element.manager=managerr;
+                if (!managerr.directs) {
+                    managerr.directs = [];
+                }
+                managerr.directs.push(element);
+            }
+            else{
+                element.manager=null;
+            }
+            if (!element.directs) {
+                element.directs = [];
+            }
+        }
+    }    
     React.useEffect(()=>{
-        fetch("/employees")
+        fetch("/api/employees")
         .then(res=>res.json())
-        .then(json=>setEmployees(json));
-    });
+        .then(json=>{updateMans(json);
+            setEmployees(json)
+        });
+    },[]);
     return(
         <main>
             <table id="dvCSV">
                 <th>Name</th><th>Title</th><th>Manager</th>
                     {
-                        employees.map(e=><tr><td>{e.name}</td><td>{e.title}</td><td>{e.manager}</td></tr>)    
+                        employees.map(e=><tr><td>{e.name}</td><td>{e.title}</td><td>{e.manager?e.manager.name:''}</td></tr>)    
                     }
             </table>
         </main>
