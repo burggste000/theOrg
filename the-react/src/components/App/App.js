b3a42b8770a0c7731d85615825b700e0b7bd9983
burggste000 <steedsranch@gmail.com>
2022-02-18 11:32:49 -0700

    just cloned repository

--- the-react/src/components/App/App.js
+++ the-react/src/components/App/App.js
@@ -12,6 +12,7 @@ export default function App() {
     //     .then((res)=>res.json())
     //     .then((json)=>setData(json));
     // });
+    // put stuff here
   return(
     <>
         <Header />
