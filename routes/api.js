47a8d8dc06a143597a235fac3dd1139557de62bf
burggste000 <steedsranch@gmail.com>
2022-02-28 12:22:06 -0700

    changed managerId to manager_id

--- routes/api.js
+++ routes/api.js
@@ -63,7 +63,7 @@ Since the majority of endpoints return the updated dataset after performing thei
 In the future, this could be implemented with promises, thus enabling the .then().catch() pattern for callers of getEmployees(...).
 */
 const getEmployees = (onGot, onError) => {
-    //const sql = "SELECT id, name, \"managerId\", title FROM Employees;"
+    //const sql = "SELECT id, name, manager_id, title FROM Employees;"
     const sql = "SELECT Employees.id, Employees.name, Employees.manager_id, Titles.title from Employees INNER JOIN Titles ON Employees.title_id = Titles.id;"
     sequelize.query(sql)
     .then((results) => {
@@ -136,25 +136,13 @@ router.post('/employees', function (req, res, next) {
 
     // The id field is "SERIAL".  This is PostreSQL specific.  Other DBs have a mechanism for creating unique, auto-increment, primary keys.  If we wanted to use the Sequelize object model functions, this could be done in a database neurtral way, but for this project we are learning SQL, so, this will only work right with PostgreSQL.
     sql += "CREATE TABLE Titles (id SERIAL PRIMARY KEY, title TEXT NOT NULL UNIQUE);";
-    sql += "CREATE TABLE Employees (id SERIAL PRIMARY KEY, name TEXT NOT NULL, 'managerId' INTEGER, title_id INTEGER NOT NULL REFERENCES Titles(id));";
-
-    /*
-    // Insert all of the values found in the post'ed request into the DB.
-    sql += "INSERT INTO Employees (id, name, \"managerId\", title_id) VALUES ";
-    let count = 0;
-    for (let e of employees) {
-        if (count++)
-            sql += ","
-        sql += "(" + e.id + ",'" + e.name + "'," + (e.managerId ? e.managerId : "0") + ",'" + e.title + "')";
-        }
-    sql += ";";
-    */
+    sql += "CREATE TABLE Employees (id SERIAL PRIMARY KEY, name TEXT NOT NULL, manager_id INTEGER, title_id INTEGER NOT NULL REFERENCES Titles(id));";
 
     // Insert all of the values found in the post-ed request into the DB.
     let count = 0;
     for (let e of employees) {
-        sql += "INSERT INTO Titles (id, title) VALUES (DEFAULT, " + e.title + ") ON CONFLICT DO NOTHING;"
-        sql += "INSERT INTO Employees (id, name, 'managerId', title_id) VALUES (" + e.id + ",'" + e.name + "'," + (e.managerId ? e.managerId : "0") + ",(SELECT id FROM Titles WHERE title = '" + e.title + "'));";
+        sql += "INSERT INTO Titles (id, title) VALUES (DEFAULT, '" + e.title + "') ON CONFLICT DO NOTHING;"
+        sql += "INSERT INTO Employees (id, name, manager_id, title_id) VALUES (" + e.id + ",'" + e.name + "'," + (e.managerId ? e.managerId : "0") + ",(SELECT id FROM Titles WHERE title = '" + e.title + "'));";
         }
 
     // Since the data uploaded has it's own id's, if future sql inserts are going to work, PostgreSQL needs to be update so that the auto id generator knows what the last id was.  See: https://dba.stackexchange.com/questions/243434/in-case-of-inserted-a-fixed-value-into-autoincrement-how-to-automatically-skip-i
@@ -176,7 +164,7 @@ router.post('/create', function (req, res, next) {
     // The insert statement does not include the id.  The database will auto-generate the id.  See the CREATE TABLE comments in router.post('/employees',...).
     // Update: Added id back into the insert statement and used DEFAULT as the value for id.  See: https://www.postgresqltutorial.com/postgresql-serial/.
     let sql = "INSERT INTO Titles (id, title) VALUES (DEFAULT, " + e.title + ") ON CONFLICT DO NOTHING;"
-    sql += "INSERT INTO Employees (id, name, 'managerId', title_id) VALUES (" + e.id + ",'" + e.name + "'," + (e.managerId ? e.managerId : "0") + ",SELECT id FROM Titles WHERE title = '" + e.title + "'));";
+    sql += "INSERT INTO Employees (id, name, manager_id, title_id) VALUES (" + e.id + ",'" + e.name + "'," + (e.managerId ? e.managerId : "0") + ",SELECT id FROM Titles WHERE title = '" + e.title + "'));";
 
     queryAndRespondWithEmployees(sql, res);
 });
@@ -191,7 +179,7 @@ The updated employee data is returned in the response.
 router.post('/delete',(req, res, next)=>{
     const id=req.body;
     let sql = "";
-    sql += "UPDATE Employees SET 'managerId' = (SELECT 'managerId' From Employees WHERE id = " + id.id + ") WHERE 'managerId' = " + id.id + ";";
+    sql += "UPDATE Employees SET manager_id = (SELECT manager_id From Employees WHERE id = " + id.id + ") WHERE manager_id = " + id.id + ";";
     sql += "DELETE FROM Employees WHERE id=" + id.id + ";";
 
     queryAndRespondWithEmployees(sql, res);
@@ -208,7 +196,7 @@ router.get('/csv', function (req, res, next) {
 
             // Next comes the employees...
             for (let e of employees)
-                csvString += e.id + "," + e.name + "," + e.managerId + "," + e.title + "\r\n";
+                csvString += e.id + "," + e.name + "," + e.manager_id + "," + e.title + "\r\n";
             res.setHeader("content-type", "text/csv");
             res.status(200).send(csvString);
         },
